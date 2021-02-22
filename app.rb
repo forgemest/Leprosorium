45b245093e4b40de74bd1b647cbf34a188aee2ec
Olek <forgemest@gmail.com>
2021-02-22 19:57:37 +0200

    Save content

--- app.rb
+++ app.rb
@@ -12,16 +12,38 @@ before do
   init_db
 end
 
+configure do
+  init_db
+
+  @db.execute 'create table if not exists Posts
+  (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_date DATE,
+    content TEXT
+  )'
+
+end
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
 
 get '/new' do
 	erb :new
 end
 
 post '/new' do
+
     content = params[:content]
+
+    if content.length <= 0
+      @error = 'Type text'
+      return erb :new
+    end
+
+    @db.execute 'insert into Posts (content, created_date) values (?, datetime())', [content]
+
   erb "You typed #{content}"
 
 end
